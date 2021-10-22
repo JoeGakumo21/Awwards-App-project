3e473e9223a394e0fbce8b8ce0c674fc8e92a48c
Joe Gakumo <joegakumo1@gmail.com>
2021-10-22 16:45:45 +0300

    create award table and make migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# creating database tables and it attributes
+class Awards(models.Model):
+    name=models.CharField(max_length=300)
+    description=models.TextField(max_length=5000)
+    created_date=models.DateField()
+    averangeRating=models.FloatField()
+    developer=models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return self.name
