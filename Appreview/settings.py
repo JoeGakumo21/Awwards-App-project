6bc786aaec44ec4e963a9c54376d34ba2e06e639
Joe Gakumo <joegakumo1@gmail.com>
2021-10-24 00:38:43 +0300

    add an edit and crispy-form to the website

--- Appreview/settings.py
+++ Appreview/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'crispy_forms',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
