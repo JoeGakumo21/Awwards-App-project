65bfef26bfc871b689af32f4ffb3c6940bf34f9d
Joe Gakumo <joegakumo1@gmail.com>
2021-10-25 01:10:45 +0300

    deploy to heroku

--- Appreview/settings.py
+++ Appreview/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
 MODE=config("MODE", default="dev")
-
+SECRET_KEY=config('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', False)
 # development
 if config('MODE')=="dev":
