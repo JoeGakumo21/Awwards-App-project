05eaccb032426826e0d021377d652481c68add9d
Joe Gakumo <joegakumo1@gmail.com>
2021-10-25 01:15:20 +0300

    deploy to hheroku

--- Appreview/settings.py
+++ Appreview/settings.py
@@ -20,7 +20,7 @@ import cloudinary_storage
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+SECRET_KEY='django-insecure-dzir@=a!=^ke)(exxac(d#12s5imul*mvu88rekgap$28$ynhv'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: don't run with debug turned on in production!
 MODE=config("MODE", default="dev")
-SECRET_KEY=config('SECRET_KEY')
+
 DEBUG = os.environ.get('DEBUG', False)
 # development
 if config('MODE')=="dev":
